b491b71cfd91ce403b0a49e7052cb06898f40dce
SzymonStolarski <szymonpiotrstolarski@gmail.com>
2022-03-04 21:56:25 +0100

    PoC version

--- components/genetic_algorithm.py
+++ components/genetic_algorithm.py
@@ -2,18 +2,99 @@ import random
 
 from components.selection import BaseSelector
 from components.crossover import BaseCrossover
+from components.mutation import BaseMutator
 
 
 class GeneticAlgorithmTSP:
     def __init__(self, population_size: int, n_iterations: int,
-                 selector: BaseSelector, crossover: BaseCrossover) -> None:
+                 selector: BaseSelector, crossover: BaseCrossover,
+                 mutator: BaseMutator, maximize: bool = False) -> None:
 
         self.population_size = population_size
         self.n_iterations = n_iterations
         self.selector = selector
         self.crossover = crossover
+        self.mutator = mutator
+        self.maximize = maximize
 
-    def create_base_population(self, city_size: int) -> dict:
+    @property
+    def results(self):
+        return self.__results
+
+    @property
+    def paths(self):
+        return self.__paths
+
+    @property
+    def best_result(self):
+        return self.__best_result
+
+    @property
+    def best_path(self):
+        return self.__best_path
+
+    def learn(self, cost_matrix: list):
+        self.__results = {}
+        self.__paths = {}
+        self.__best_result = 0
+        self.__best_path = []
+
+        iteration = 0
+        population = self._create_base_population(len(cost_matrix))
+        evaluated_population = self._evaluate(population, cost_matrix)
+
+        if not self.maximize:
+            self.__results[iteration] = min(list(evaluated_population.values()))
+            self.__paths[iteration] = population[min(evaluated_population,
+                                                     key=evaluated_population.get)
+                                                 ]
+        else:
+            self.__results[iteration] = max(list(evaluated_population.values()))
+            self.__paths[iteration] = population[max(evaluated_population,
+                                                     key=evaluated_population.get)
+                                                 ]
+        print(f'Iteration: {iteration}. Best result: {self.__results[iteration]}')
+
+        while not iteration == self.n_iterations:
+            # Increment iteration here because iteration 0 was base population
+            iteration += 1
+
+            # Selection
+            selected_population = self.selector.selection(population,
+                                                          evaluated_population,
+                                                          self.maximize)
+            # Mutation
+            population = self.mutator.mutate(selected_population)
+            # Crossover
+            population = self.crossover.crossover(population)
+            # Evaluation
+            evaluated_population = self._evaluate(population, cost_matrix)
+
+            if not self.maximize:
+                self.__results[iteration] = min(list(evaluated_population.values()))
+                self.__paths[iteration] = population[min(evaluated_population,
+                                                        key=evaluated_population.get)
+                                                     ]
+            else:
+                self.__results[iteration] = max(list(evaluated_population.values()))
+                self.__paths[iteration] = population[max(evaluated_population,
+                                                        key=evaluated_population.get)
+                                                     ]
+
+            print(f'Iteration: {iteration}. Best result: {self.__results[iteration]}')
+
+        if not self.maximize:
+            self.__best_result = min(list(self.__results.values()))
+            self.__best_path = self.__paths[min(self.__results,
+                                                key=self.__results.get)]
+        else:
+            self.__best_result = min(list(self.__results.values()))
+            self.__best_path = self.__paths[min(self.__results,
+                                                key=self.__results.get)]
+
+        return self
+
+    def _create_base_population(self, city_size: int) -> dict:
         base_population = {}
 
         for i in range(self.population_size):
@@ -24,7 +105,7 @@ class GeneticAlgorithmTSP:
 
         return base_population
 
-    def evaluate(self, population: dict, cost_data: list) -> dict:
+    def _evaluate(self, population: dict, cost_matrix: list) -> dict:
         population_score = {}
 
         for index, values in population.items():
@@ -52,14 +133,11 @@ class GeneticAlgorithmTSP:
                     # Calculate the full cost for given individual
                     # Takes the cost of each route from the cost matrix
                     # Not the prettiest syntax, however quite fast solution
-                    score = sum([cost_data[coordinates[0]][coordinates[1]]
+                    score = sum([cost_matrix[coordinates[0]][coordinates[1]]
                                 if coordinates[0] > coordinates[1]
-                                else cost_data[coordinates[1]][coordinates[0]]
+                                else cost_matrix[coordinates[1]][coordinates[0]]
                                 for coordinates in coordinates_list])
 
                     population_score[index] = score
 
         return population_score
-
-        def learn(self):
-            pass
--- components/selection.py
+++ components/selection.py
@@ -5,22 +5,36 @@ import random
 class BaseSelector(ABC):
 
     @abstractmethod
-    def selection(self, evaluated_population) -> list:
+    def selection(self, population: dict, evaluated_population: dict,
+                  maximize: bool = False) -> list:
         return
+
+    def _select_population(self, population: dict,
+                           selected_individuals_keys: list):
+
+        selected_population_vals = [population[i] for i
+                                    in selected_individuals_keys
+                                    if i in population.keys()]
+        selected_population = {k: v for k, v in zip(range(0,
+                               len(selected_individuals_keys)),
+                               selected_population_vals)}
+
+        return selected_population
 
 
 class ElitismSelector(BaseSelector):
 
-    def __init__(self, percent: float, maximize: bool = False) -> None:
+    def __init__(self, percent: float) -> None:
         self.percent = percent
-        self.maximize = maximize
 
-    def selection(self, evaluated_population: dict) -> list:
+    def selection(self, population: dict, evaluated_population: dict,
+                  maximize: bool = False) -> dict:
+
         no_of_elite_individuals = (int(len(
                                    evaluated_population)*self.percent))
         sorted_evaluation = {k: v for k, v in sorted(
                              evaluated_population.items(),
-                             key=lambda item: item[1], reverse=self.maximize)}
+                             key=lambda item: item[1], reverse=maximize)}
         best_individuals = [k for k in
                             list(
                                  sorted_evaluation.keys()
@@ -30,50 +44,59 @@ class ElitismSelector(BaseSelector):
         missing_multiplier = int(
                             len(evaluated_population)/no_of_elite_individuals
                                 )
-        selected_individuals = missing_multiplier*best_individuals
-        if len(selected_individuals) < len(evaluated_population):
-            difference = (len(evaluated_population)-len(selected_individuals))
-            selected_individuals.extend(selected_individuals[:difference])
+        selected_individuals_keys = missing_multiplier*best_individuals
+        if len(selected_individuals_keys) < len(evaluated_population):
+            difference = (len(evaluated_population)-len(selected_individuals_keys))
+            selected_individuals_keys.extend(selected_individuals_keys[:difference])
 
-        return selected_individuals
+        selected_population = self._select_population(population,
+                                                      selected_individuals_keys)
+
+        return selected_population
 
 
 class TournamentSelector(BaseSelector):
 
-    def __init__(self, k_percent: float, maximize: bool = False) -> None:
+    def __init__(self, k_percent: float) -> None:
         self.k_percent = k_percent
-        self.maximize = maximize
 
-    def selection(self, evaluated_population: dict) -> list:
+    def selection(self, population: dict, evaluated_population: dict,
+                  maximize: bool = False) -> dict:
+
         population_size = len(evaluated_population)
         no_of_ind_in_tournament = int(self.k_percent*population_size)
 
-        selected_individuals = []
+        selected_individuals_keys = []
         iterator = 0
         while not iterator == len(evaluated_population):
             selected_keys = random.sample(
                             range(population_size), no_of_ind_in_tournament
                             )
             tournament = {k: evaluated_population[k] for k in selected_keys}
-            if not self.maximize:
+            if not maximize:
                 winner = min(tournament, key=tournament.get)
             else:
                 winner = max(tournament, key=tournament.get)
 
-            selected_individuals.append(winner)
+            selected_individuals_keys.append(winner)
             iterator += 1
 
-        return selected_individuals
+        selected_population = self._select_population(population,
+                                                      selected_individuals_keys)
+
+        return selected_population
 
 
 class RouletteSelector(BaseSelector):
 
-    def __init__(self, maximize: bool = False) -> None:
-        self.maximize = maximize
+    def __init__(self) -> None:
+        pass
 
-    def selection(self, evaluated_population: dict) -> list:
+    def selection(self, population: dict, evaluated_population: dict,
+                  maximize: bool = False) -> dict:
+
         sum_of_scores = sum([v for v in evaluated_population.values()])
-        if not self.maximize:
+        if not maximize:
             dict_of_share_initial = {k: v/sum_of_scores for k, v
                                      in evaluated_population.items()}
             dict_of_share_sorted = {k: v for k, v in sorted(
@@ -102,7 +125,7 @@ class RouletteSelector(BaseSelector):
             ranges.append(current_range)
         dict_with_ranges = {k: v for k, v in zip(dict_of_share.keys(), ranges)}
 
-        selected_individuals = []
+        selected_individuals_keys = []
         iterator = 0
         while not iterator == len(evaluated_population):
             random_number = random.random()
@@ -110,7 +133,10 @@ class RouletteSelector(BaseSelector):
                                    if random_number > v[0]
                                    and random_number <= v[1]
                                    ][0]
-            selected_individuals.append(selected_individual)
+            selected_individuals_keys.append(selected_individual)
             iterator += 1
 
-        return selected_individuals
+        selected_population = self._select_population(population,
+                                                      selected_individuals_keys)
+
+        return selected_population
